5c3b8c7c13f95aeb8a02aeac0208aabd6c1871e0
Henrique Tomaz <henrique.tomaz@fagammon.edu.br>
2025-02-12 10:35:11 -0300

    prepare deploy

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,13 @@ import knex from 'knex'
 import { env } from './env/env-zod'
 
 export const config: knex.Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
